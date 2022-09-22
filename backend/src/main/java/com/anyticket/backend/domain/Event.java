cddf9d1915dbcce7152aafe285547abbbf3459a2
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-09-22 22:01:38 +0300

    [#1] Added User domain class

---
+++ backend/src/main/java/com/anyticket/backend/domain/Event.java
@@ -0,0 +1,17 @@
+package com.anyticket.backend.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Id;
+
+@Data
+@RequiredArgsConstructor
+public class Event {
+
+    @Id
+    private long id;
+
+    @
+
+}
---
+++ backend/src/main/java/com/anyticket/backend/domain/User.java
@@ -0,0 +1,34 @@
+package com.anyticket.backend.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@ToString
+@Entity(name = "user")
+@RequiredArgsConstructor
+public class User {
+
+    @Id
+    private long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
+}
