906f496a099d17385a371fa5aa7a6c8934d0d20f
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-09-23 13:38:07 +0300

    [#1] Created rest controller for User entity

---
+++ backend/src/main/java/com/anyticket/backend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.anyticket.backend.controller;
+
+import com.anyticket.backend.dto.UserDto;
+import com.anyticket.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Set<UserDto>> getUsers() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+}
