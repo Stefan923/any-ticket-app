3ff1cf92519d072c7938c5e46cd04c0c4f04c8cb
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-10-11 14:22:35 +0300

    [#1] Implemented event service interface

---
+++ backend/src/main/java/com/anyticket/backend/service/EventServiceImpl.java
@@ -0,0 +1,38 @@
+package com.anyticket.backend.service;
+
+import com.anyticket.backend.domain.Event;
+import com.anyticket.backend.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class EventServiceImpl implements EventService {
+
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventServiceImpl(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    @Transactional
+    public List<Event> findAll(int pageNumber, int pageSize, String sortBy) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy));
+        Page<Event> page = eventRepository.findAll(pageable);
+
+        if (page.hasContent()) {
+            return page.getContent();
+        }
+        return Collections.emptyList();
+    }
+
+}
